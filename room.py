2d9f4dc7c06cf8a010559bd51d62fd3ec98c3631
Napassadol Seangchandao <napassadol.s@gmail.com>
2019-04-23 20:38:14 +0700

    update bank

--- room.py
+++ room.py
@@ -95,7 +95,7 @@ class CheckCash(threading.Thread):
         while self.enable:
             if (time.time() - pluse_time > 1) and (count != 0):
                 cash_bank = 0
-                print('Count: ' + count)
+                print('Count: ' + str(count))
                 if count <= 40 and count > 30:
                     print(count)
                     print('100')
